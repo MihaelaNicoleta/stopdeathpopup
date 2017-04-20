f2e25138c653c49215f85b080af84a0b2438e0fc
MihaelaNicoleta <raumiha21@gmail.com>
2017-04-20 22:47:11 +0300

    Initial commit

---
+++ StopDeathPopup/app/src/main/java/com/example/miha/stopdeathpopup/StopDeathPopup.java
@@ -0,0 +1,13 @@
+package com.example.miha.stopdeathpopup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopDeathPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_death_popup);
+    }
+}
