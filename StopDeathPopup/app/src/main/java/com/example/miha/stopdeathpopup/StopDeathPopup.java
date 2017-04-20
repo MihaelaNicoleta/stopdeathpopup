13b4ec60b03a1e3621ea6d34acfb2ba78a09be04
MihaelaNicoleta <raumiha21@gmail.com>
2017-04-20 22:53:01 +0300

    Display popup on load

--- StopDeathPopup/app/src/main/java/com/example/miha/stopdeathpopup/StopDeathPopup.java
+++ StopDeathPopup/app/src/main/java/com/example/miha/stopdeathpopup/StopDeathPopup.java
@@ -1,13 +1,30 @@
 package com.example.miha.stopdeathpopup;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class StopDeathPopup extends AppCompatActivity {
+
+    Button yesButton;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stop_death_popup);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        dialog = new Dialog(this);
+
+        dialog.setContentView(R.layout.activity_stop_death_popup);
+        yesButton = (Button) dialog.findViewById(R.id.yes_button);
+        dialog.show();
     }
+
 }
